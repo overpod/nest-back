53686e82a5e8ab47cb047ef4dea2a33ce8465671
Trofimov Alexey <overpod@yandex.ru>
2022-01-15 07:51:40 +0300

    change test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,16 +1,87 @@
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { getConnection, Connection } from 'typeorm';
+
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('e2e', () => {
+  let app: NestFastifyApplication;
+  let connection: Connection;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const appModule: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = appModule.createNestApplication(new FastifyAdapter());
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+    connection = getConnection();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  afterEach(async () => {
+    const entities = connection.entityMetadatas;
+    for (const entity of entities) {
+      await connection.getRepository(entity.name).clear();
+    }
+  });
+
+  describe('auth', () => {
+    const user = {
+      email: 'test@test.com',
+      password: 'testTestingPassword',
+    };
+    it('SignUp User', async () =>
+      app
+        .inject()
+        .post('/auth/signup')
+        .body(user)
+        .then((result) => {
+          expect(result.statusCode).toEqual(201);
+          expect(result.json()).toEqual({
+            token: expect.any(String),
+            user: {
+              id: expect.any(String),
+              createdAt: expect.any(String),
+              updatedAt: expect.any(String),
+              email: expect.any(String),
+            },
+          });
+        }));
+
+    it('SignUp User already exists', async () => {
+      await app
+        .inject()
+        .post('/auth/signup')
+        .body(user)
+        .then((result) => {
+          expect(result.statusCode).toEqual(201);
+          expect(result.json()).toEqual({
+            token: expect.any(String),
+            user: {
+              id: expect.any(String),
+              createdAt: expect.any(String),
+              updatedAt: expect.any(String),
+              email: expect.any(String),
+            },
+          });
+        });
+      await app
+        .inject()
+        .post('/auth/signup')
+        .body(user)
+        .then((result) => {
+          expect(result.json()).toEqual({
+            statusCode: 400,
+            message: 'User already exists',
+          });
+        });
+    });
   });
 });
